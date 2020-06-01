cc06802169d31386ffce79c18a120778a70155f6
Rutger Broekhoff <rutger@viasalix.nl>
2020-06-01 22:33:30 +0200

    frontend-embedded: actually working

--- frontend-embedded/cardreadercontroller.cpp
+++ frontend-embedded/cardreadercontroller.cpp
@@ -5,14 +5,19 @@ CardReaderController::CardReaderController(CardReader *cardReader,
     : QObject(parent), m_cardReader(cardReader)
 {
     m_cardReader->moveToThread(&cardReaderThread);
+
     connect(&cardReaderThread, &QThread::finished, m_cardReader, &QObject::deleteLater);
     connect(m_cardReader, &CardReader::cardRead, this, &CardReaderController::cardRead);
     connect(this, &CardReaderController::runCardReader, m_cardReader, &CardReader::start);
+
+    cardReaderThread.start();
+
     emit runCardReader();
 }
 
 CardReaderController::~CardReaderController() {
     m_cardReader->shutDown();
+
     cardReaderThread.quit();
     cardReaderThread.wait();
 }
