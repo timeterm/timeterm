7d76a45ce42eb22cfd00680a0078279d72f7d42e
Robert van der Maas <robertvdmaas3@gmail.com>
2020-08-28 11:23:17 +0000

    Update zermelo.go

--- backend/integration/zermelo/zermelo.go
+++ backend/integration/zermelo/zermelo.go
@@ -1,1 +1,32 @@
 package zermelo
+
+import (
+    "net/http"
+    "github.com/go-logr/logr"
+)
+
+type Client struct {
+    log logr.Logger
+}
+
+type StudentClient struct {
+    client Client
+}
+
+type Appointment struct {
+    
+}
+
+func (c *Client) getAppointments(token string, year, week int) (*[]Appointment, error) {
+    resp, err := http.Get("https://{institution}.zportal.nl/api/v3/liveschedule?student={student}&week={week}")
+    if err != nil {
+        return nil, err
+    }
+
+    defer func() {
+        err = resp.Body.Close()
+        if err != nil {
+            return nil, err
+        }
+    }()
+}
