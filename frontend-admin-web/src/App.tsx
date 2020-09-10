b08df35188018b0a98f2db042c285183cb165dcd
Rutger Broekhoff <rutger@viasalix.nl>
2020-09-10 20:33:29 +0200

    frontend-admin-web: try creating a drawer

--- frontend-admin-web/src/App.tsx
+++ frontend-admin-web/src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "@rmwc/drawer/dist/styles";
+import "@rmwc/list/dist/styles";
+import "@rmwc/button/dist/styles";
+import "@rmwc/icon/dist/styles";
+import "@rmwc/elevation/dist/styles";
+import { Drawer, DrawerContent, DrawerHeader } from "@rmwc/drawer";
+import { List, ListItem } from "@rmwc/list";
+import { Icon } from "@rmwc/icon";
+import { Elevation } from "@rmwc/elevation";
+import Logo from "./logo-white.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Elevation z={24}>
+        <Drawer
+          style={{
+            backgroundColor: "rgba(57, 156, 248, 1)",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <DrawerHeader>
+            <img src={Logo} alt={"Timeterm Logo"} width={96} />
+          </DrawerHeader>
+          <DrawerContent>
+            <List>
+              <ListItem style={{ color: "white" }}>
+                <Icon icon="tablet" />
+                &nbsp; Apparaten
+              </ListItem>
+              <ListItem style={{ color: "white" }}>
+                <Icon icon="group" />
+                &nbsp; Gebruikers
+              </ListItem>
+              <ListItem style={{ color: "white" }}>
+                <Icon icon="bluetooth" />
+                &nbsp; Apparaat koppelen
+              </ListItem>
+            </List>
+          </DrawerContent>
+        </Drawer>
+      </Elevation>
     </div>
   );
 }
