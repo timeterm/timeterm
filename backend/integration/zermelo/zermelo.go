d979f2d4be0c28a417c21e3e1c4cf523163daaac
Rutger Broekhoff <rutger@viasalix.nl>
2020-09-14 07:55:06 +0200

    be/integration/zermelo: modify StudentClient#getAppointments to use token

--- backend/integration/zermelo/zermelo.go
+++ backend/integration/zermelo/zermelo.go
@@ -1,45 +1,73 @@
 package zermelo
 
 import (
-	"net/url"
+	"context"
 	"fmt"
-    "net/http"
-    "github.com/go-logr/logr"
+	"net/http"
+	"net/url"
+
+	"github.com/go-logr/logr"
 )
 
 type Client struct {
-    log logr.Logger
+	log logr.Logger
 }
 
 type StudentClient struct {
-	client Client
+	client      Client
 	institution string
 	studentCode string
 }
 
 type Appointment struct {
-    
 }
 
-func (sc *StudentClient) getAppointments(token string, year, week int) ([]Appointment, error) {
-	baseURLStr := fmt.Sprintf("https://%s.zportal.nl/api/v3/liveschedule", sc.institution)
-	baseURL, err := url.Parse(baseURLStr)
+type tokenRoundTripper struct {
+	token string
+	next  http.RoundTripper
+}
+
+func (t tokenRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Set("Authorization", "Bearer "+t.token)
+
+	return t.next.RoundTrip(r)
+}
+
+func withToken(token string, hc *http.Client) *http.Client {
+	c := *hc
+	c.Transport = tokenRoundTripper{
+		token: token,
+		next:  c.Transport,
+	}
+
+	return &c
+}
+
+func (sc *StudentClient) getAppointments(ctx context.Context, token string, year, week int) ([]Appointment, error) {
+	baseURL := fmt.Sprintf("https://%s.zportal.nl/api/v3/liveschedule", sc.institution)
+	reqURL, err := url.Parse(baseURL)
 	if err != nil {
 		return nil, err
 	}
-	q := baseURL.Query()
+
+	q := reqURL.Query()
 	q.Set("student", sc.studentCode)
 	q.Set("week", fmt.Sprintf("%4d%02d", year, week))
-	baseURL.RawQuery = q.Encode()
-    resp, err := http.Get(baseURL.String())
-    if err != nil {
-        return nil, err
-    }
+	reqURL.RawQuery = q.Encode()
 
-    defer func() {
-        err = resp.Body.Close()
-        if err != nil {
-            sc.client.log.Error(err, "Failed to close response body in getAppointments")
-        }
-    }()
+	req := http.Request{
+		Method: http.MethodGet,
+		URL:    reqURL,
+	}
+
+	resp, err := withToken(token, http.DefaultClient).Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err = resp.Body.Close()
+		if err != nil {
+			sc.client.log.Error(err, "Failed to close response body in getAppointments")
+		}
+	}()
 }
