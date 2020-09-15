d19fe43cd0f9bace0f5bb88ed8e21de9e2f0ba00
Rutger Broekhoff <rutger@viasalix.nl>
2020-09-15 21:00:05 +0200

    be: disable verbose mode for NATS in docker-compose.yml

--- backend/jsmtest/jsm_test.go
+++ backend/jsmtest/jsm_test.go
@@ -6,12 +6,15 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
+	"gitlab.com/timeterm/timeterm/proto/go"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestJSMGetMsg(t *testing.T) {
-	conn, err := nats.Connect("localhost", nats.UseOldRequestStyle())
+	conn, err := nats.Connect("localhost")
 	if err != nil {
 		t.Fatalf("Could not connect to NATS: %v", err)
 	}
@@ -28,17 +31,24 @@ func TestJSMGetMsg(t *testing.T) {
 		go func() {
 			defer sema.Release(1)
 
-			_, err = conn.Request("FEDEV.ozuhLrexlBa4p50INjihAl.DISOWN-TOKEN", []byte("TEST"), time.Second*10)
+			bs, err := proto.Marshal(&timetermpb.DisownTokenMessage{
+				DeviceId: uuid.New().String(),
+			})
+			if err != nil {
+				t.Fatalf("Could not marshal DisownTokenMessage: %v", err)
+			}
+
+			_, err = conn.Request("FEDEV.ozuhLrexlBa4p50INjihAl.DISOWN-TOKEN", bs, time.Second*10)
 			if err != nil {
 				t.Fatalf("Could not publish message to stream: %v", err)
 			}
 		}()
 
-		if i % 1000 == 0 {
-			fmt.Printf("%d/1000\n", i / 1000)
+		if i%1000 == 0 {
+			fmt.Printf("%d/1000\n", i/1000)
 		}
 	}
 
-	sema.Acquire(context.Background(), 1000)
+	_ = sema.Acquire(context.Background(), 1000)
 	fmt.Printf("1 mil messages produced in %s\n", time.Since(start))
 }
