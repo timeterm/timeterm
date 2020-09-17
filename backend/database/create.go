8215ffd0dbf9c821b05f28ac6d7820022982109c
Robertvdmaas <robertvdmaas3@gmail.com>
2020-09-17 20:22:22 +0200

    be: add CreateStudent and CreateDevice in database/create.go

--- backend/database/create.go
+++ backend/database/create.go
@@ -11,6 +11,18 @@ type Organization struct {
 	Name string    `db:"name"`
 }
 
+type Student struct {
+	ID             uuid.UUID `db:"id"`
+	OrganizationID uuid.UUID `db:"organization_id"`
+}
+
+type Device struct {
+	ID             uuid.UUID `db:"id"`
+	OrganizationID uuid.UUID `db:"organization_id"`
+	Name           string    `db:"name"`
+	Status         string    `db:"status"`
+}
+
 func (w *Wrapper) CreateOrganization(ctx context.Context, name string) (Organization, error) {
 	org := Organization{
 		Name: name,
@@ -19,4 +31,26 @@ func (w *Wrapper) CreateOrganization(ctx context.Context, name string) (Organiza
 	row := w.db.QueryRowContext(ctx, `INSERT INTO "organization" ("id", "name") VALUES (DEFAULT, $1) RETURNING "id"`, name)
 
 	return org, row.Scan(&org.ID)
+}
+
+func (w *Wrapper) CreateStudent(ctx context.Context, organizationID uuid.UUID) (Student, error) {
+	std := Student{
+		OrganizationID: organizationID,
+	}
+
+	row := w.db.QueryRowContext(ctx, `INSERT INTO "student" ("id", "organizationID") VALUES (DEFAULT, $1) RETURNING "id"`, organizationID)
+
+	return std, row.Scan(&std.ID)
+}
+
+func (w *Wrapper) CreateDevice(ctx context.Context, organizationID uuid.UUID, name string, status string) (Device, error) {
+	dev := Device{
+		OrganizationID: organizationID,
+		Name:           name,
+		Status:         status,
+	}
+
+	row := w.db.QueryRowContext(ctx, `INSERT INTO "device" ("id", "organizationID", "name", "status") VALUES (DEFAULT, $1, $2, $3) RETURNING "id"`, organizationID, name, status)
+
+	return dev, row.Scan(&dev.ID)
 }
