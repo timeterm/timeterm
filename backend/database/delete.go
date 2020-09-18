ef6bdddb2a0d5537114dbf55c02e9a11e4cc28e0
Robert van der Maas <robertvdmaas3@gmail.com>
2020-09-18 10:59:09 +0000

    be: fix delete queries

--- backend/database/delete.go
+++ backend/database/delete.go
@@ -7,13 +7,16 @@ import (
 )
 
 func (w *Wrapper) DeleteOrganization(ctx context.Context, id uuid.UUID) error {
-	return w.db.ExecContext(ctx, `DELETE FROM "organization" WHERE "id" = $1`, id)
+	_, err := w.db.ExecContext(ctx, `DELETE FROM "organization" WHERE "id" = $1`, id)
+	return err
 }
 
 func (w *Wrapper) DeleteStudent(ctx context.Context, id uuid.UUID) error {
-	return w.db.ExecContext(ctx, `DELETE FROM "student" WHERE "id" = $1`, id)
+	_, err := w.db.ExecContext(ctx, `DELETE FROM "student" WHERE "id" = $1`, id)
+	return err
 }
 
 func (w *Wrapper) DeleteDevice(ctx context.Context, id uuid.UUID) error {
-	return w.db.ExecContext(ctx, `DELETE FROM "device" WHERE "id" = $1`, id)
+	_, err := w.db.ExecContext(ctx, `DELETE FROM "device" WHERE "id" = $1`, id)
+	return err
 }
