953201ecbc76a140447b01233398ceb71709d07e
Robert van der Maas <robertvdmaas3@gmail.com>
2020-11-12 12:26:34 +0000

    be: update secrets.go

--- backend/secrets/secrets.go
+++ backend/secrets/secrets.go
@@ -1,20 +1,71 @@
 package secrets
 
 import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
 	"github.com/hashicorp/vault/api"
-	"github.com/go-logr/logr"
+	"google.golang.org/protobuf/proto"
+
+	devcfgpb "gitlab.com/timeterm/timeterm/proto/go/devcfg"
 )
 
-type Logger struct {
-	log logr.Logger
+type Wrapper struct {
+	c *api.Client
 }
 
-func  (l *Logger) createClient() error {
+func New() (*Wrapper, error) {
 	client, err := api.NewClient(api.DefaultConfig())
 	if err != nil {
-		l.log.Error(err, "could not create client")
+		return nil, err
+	}
+	return &Wrapper{client}, nil
+}
+
+func createSecretPath(id uuid.UUID) string {
+	return fmt.Sprintf("/timeterm/timeterm/ethernet/config/%s", id)
+}
+
+func (w *Wrapper) GetEthernetConfig(id uuid.UUID) (*devcfgpb.EthernetService, error) {
+	secretPath := createSecretPath(id)
+	secret, err := w.c.Logical().Read(secretPath)
+	if err != nil {
+		return nil, err
 	}
 
-	logicalClient := client.Logical()
-	logicalClient.Read()
+	bytes, ok := secret.Data["config"].(string)
+	if !ok {
+		return nil, errors.New("could not retrieve config from secret")
+	}
+
+	decoded, err := base64.StdEncoding.DecodeString(bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	var ethernetService devcfgpb.EthernetService
+
+	err = proto.Unmarshal(decoded, &ethernetService)
+	if err != nil {
+		return nil, err
+	}
+	return &ethernetService, nil
+}
+
+func (w *Wrapper) upsertEthernetConfig(id uuid.UUID, cfg *devcfgpb.EthernetService) error {
+	bytes, err := proto.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	encoded := base64.StdEncoding.EncodeToString(bytes)
+
+	secretPath := createSecretPath(id)
+
+	_, err = w.c.Logical().Write(secretPath, map[string]interface{}{
+		"config": encoded,
+	})
+	return err
 }
