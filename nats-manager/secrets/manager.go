602c89b90d3b97abfe756558dc4718aa27430fa9
Rutger Broekhoff <rutger@viasalix.nl>
2020-11-19 07:58:17 +0100

    nm: clean up a tiny little bit

--- nats-manager/secrets/manager.go
+++ nats-manager/secrets/manager.go
@@ -18,14 +18,74 @@ func (d *Manager) Init() error {
 	return d.InitKeys()
 }
 
-func (d *Manager) InitKeys() error {
-	sakp, err := nkeys.CreateAccount()
+func (d *Manager) newAccountKeys() (nkeys.KeyPair, error) {
+	kp, err := nkeys.CreateAccount()
 	if err != nil {
-		return fmt.Errorf("could not create system account keys: %w", err)
+		return kp, fmt.Errorf("could not generate account keys: %w", err)
 	}
 
-	if err = d.safe.WriteAccountSeed(sakp); err != nil {
-		return fmt.Errorf("could not write system account seed: %w", err)
+	if err = d.safe.WriteAccountSeed(kp); err != nil {
+		return kp, fmt.Errorf("could not write account seed: %w", err)
+	}
+
+	return kp, nil
+}
+
+func (d *Manager) newOperatorKeys() (nkeys.KeyPair, error) {
+	kp, err := nkeys.CreateOperator()
+	if err != nil {
+		return kp, fmt.Errorf("could not generate operator keys: %w", err)
+	}
+
+	if err = d.safe.WriteOperatorSeed(kp); err != nil {
+		return kp, fmt.Errorf("could not write operator seed: %w", err)
+	}
+
+	return kp, nil
+}
+
+func (d *Manager) newUserKeys() (nkeys.KeyPair, error) {
+	kp, err := nkeys.CreateUser()
+	if err != nil {
+		return kp, fmt.Errorf("could not generate user keys: %w", err)
+	}
+
+	if err = d.safe.WriteUserSeed(kp); err != nil {
+		return kp, fmt.Errorf("could not write user seed: %w", err)
+	}
+
+	return kp, nil
+}
+
+func (d *Manager) newOperator(systemAccountPubKey string) (string, error) {
+	opkp, err := d.newOperatorKeys()
+	if err != nil {
+		return "", fmt.Errorf("could not create operator keys: %w", err)
+	}
+
+	oppk, err := opkp.PublicKey()
+	if err != nil {
+		return "", fmt.Errorf("could not create operator public key: %w", err)
+	}
+
+	oclaims := jwt.NewOperatorClaims(oppk)
+	oclaims.Name = d.operatorName
+	oclaims.Issuer = oppk
+	oclaims.IssuedAt = time.Now().Unix()
+	oclaims.SystemAccount = systemAccountPubKey
+
+	err = d.safe.WriteOperatorJWT(oclaims, oppk)
+	if err != nil {
+		return oppk, fmt.Errorf("could not write operator JWT: %w", err)
+	}
+
+	return oppk, nil
+}
+
+func (d *Manager) InitKeys() error {
+	sakp, err := d.newAccountKeys()
+	if err != nil {
+		return fmt.Errorf("could not create system account keys: %w", err)
 	}
 
 	sapk, err := sakp.PublicKey()
@@ -33,57 +93,19 @@ func (d *Manager) InitKeys() error {
 		return fmt.Errorf("could not create system account public key: %w", err)
 	}
 
-	opkp, err := nkeys.CreateOperator()
+	oppk, err := d.newOperator(sapk)
 	if err != nil {
-		return fmt.Errorf("could not create operator keys: %w", err)
+		return fmt.Errorf("could not create operator: %w", err)
 	}
 
-	if err = d.safe.WriteOperatorSeed(opkp); err != nil {
-		return fmt.Errorf("could not write operator seed: %w", err)
-	}
-
-	oppk, err := opkp.PublicKey()
-	if err != nil {
-		return fmt.Errorf("could not create operator public key: %w", err)
-	}
-
-	oclaims := jwt.NewOperatorClaims(oppk)
-	oclaims.Name = d.operatorName
-	oclaims.Issuer = oppk
-	oclaims.IssuedAt = time.Now().Unix()
-	oclaims.SystemAccount = sapk
-
-	err = d.safe.WriteOperatorJWT(oclaims, oppk)
-	if err != nil {
-		return fmt.Errorf("could not write operator JWT: %w", err)
-	}
-
-	oakp, err := nkeys.CreateAccount()
+	oakp, err := d.newAccountKeys()
 	if err != nil {
 		return fmt.Errorf("could not create operator account keys: %w", err)
-	}
-
-	if err = d.safe.WriteAccountSeed(oakp); err != nil {
-		return fmt.Errorf("could not write operator account seed: %w", err)
 	}
 
 	oapk, err := oakp.PublicKey()
 	if err != nil {
 		return fmt.Errorf("could not create operator account public key: %w", err)
-	}
-
-	oukp, err := nkeys.CreateUser()
-	if err != nil {
-		return fmt.Errorf("could not create operator user keys: %w", err)
-	}
-
-	if err = d.safe.WriteUserSeed(oukp); err != nil {
-		return fmt.Errorf("could not write operator user seed: %w", err)
-	}
-
-	oupk, err := oukp.PublicKey()
-	if err != nil {
-		return fmt.Errorf("could not create operator user public key: %w", err)
 	}
 
 	oaclaims := jwt.NewAccountClaims(oapk)
@@ -94,6 +116,16 @@ func (d *Manager) InitKeys() error {
 	err = d.safe.WriteAccountJWT(oaclaims, oppk)
 	if err != nil {
 		return fmt.Errorf("could not write operator account JWT: %w", err)
+	}
+
+	oukp, err := d.newUserKeys()
+	if err != nil {
+		return fmt.Errorf("could not create operator user keys: %w", err)
+	}
+
+	oupk, err := oukp.PublicKey()
+	if err != nil {
+		return fmt.Errorf("could not create operator user public key: %w", err)
 	}
 
 	ouclaims := jwt.NewUserClaims(oupk)
@@ -116,13 +148,9 @@ func (d *Manager) InitKeys() error {
 		return fmt.Errorf("could not write system account JWT: %w", err)
 	}
 
-	sukp, err := nkeys.CreateUser()
+	sukp, err := d.newUserKeys()
 	if err != nil {
 		return fmt.Errorf("could not create system user keys: %w", err)
-	}
-
-	if err = d.safe.WriteUserSeed(sukp); err != nil {
-		return fmt.Errorf("could not write system user seed: %w", err)
 	}
 
 	supk, err := sukp.PublicKey()
