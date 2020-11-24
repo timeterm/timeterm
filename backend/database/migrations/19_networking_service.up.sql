4f1ce407b342523f4fc72454598a3a50911ef4d1
Rutger Broekhoff <rutger@viasalix.nl>
2020-11-24 23:11:03 +0100

    be: fix migration 19

--- backend/database/migrations/19_networking_service.up.sql
+++ backend/database/migrations/19_networking_service.up.sql
@@ -1,10 +1,10 @@
 BEGIN;
 
-CREATE TABLE networking_service 
+CREATE TABLE networking_service
 (
-    "id"                uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    "organization_id"   uuid NOT NULL,
-    "name"              string NOT NULL,
+    "id"              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    "organization_id" uuid NOT NULL,
+    "name"            text NOT NULL,
 
     FOREIGN KEY (organization_id) REFERENCES organization (id) ON DELETE CASCADE
 );
