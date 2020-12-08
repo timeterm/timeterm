7b59e2620e0ff4e5d8a08fc4a00ea85fb136f736
Rutger Broekhoff <rutgerbroekhoff3@gmail.com>
2020-12-08 11:32:19 +0000

    fe: log error when systemd reboot does not work

--- frontend-embedded/src/cpp/systemd/systemd.cpp
+++ frontend-embedded/src/cpp/systemd/systemd.cpp
@@ -8,6 +8,14 @@ Systemd::Systemd(QObject *parent)
 void Systemd::rebootDevice() {
 #ifdef TIMETERMOS
     auto manager = org::freedesktop::systemd1::Manager("org.freedesktop.systemd1", "/org/freedesktop/systemd1", QDBusConnection::systemBus());
-    manager.Reboot();
+    auto reply = manager.Reboot();
+
+    reply.waitForFinished();
+
+    qDebug() << "Rebooting...";
+    if (reply.isError())
+        qCritical() << "Could not reboot:" << reply.error().message();
+    else
+        qDebug() << "Successfully issued reboot command to systemd";
 #endif
 }
